ea2c2965a9a0800f6e8aa814908ed0beafc78057
Yuri_sv <travin@hotbox.ru>
2016-12-06 11:40:32 +0300

    Added Hard Fault Handler that reads the state of the ARM Cortex-M registers at the time that the fault occurred  from the stack.

--- os/common/ports/ARMCMx/compilers/GCC/vectors.c
+++ os/common/ports/ARMCMx/compilers/GCC/vectors.c
@@ -78,12 +78,71 @@ void _unhandled_exception(void) {
   }
 }
 
+/* The prototype shows it is a naked function - in effect this is just an
+assembly function. */
+void _HardFault_exception( void ) __attribute__( ( naked ) );
+
+/* The fault handler implementation calls a function called
+prvGetRegistersFromStack(). */
+void _HardFault_exception(void)
+{
+    __asm volatile
+    (
+        " tst lr, #4                                                \n"
+        " ite eq                                                    \n"
+        " mrseq r0, msp                                             \n"
+        " mrsne r0, psp                                             \n"
+        " ldr r1, [r0, #24]                                         \n"
+        " ldr r2, handler2_address_const                            \n"
+        " bx r2                                                     \n"
+        " handler2_address_const: .word prvGetRegistersFromStack    \n"
+    );
+}
+
+void prvGetRegistersFromStack( uint32_t *pulFaultStackAddress ) __attribute__( ( used ) );
+
+void prvGetRegistersFromStack( uint32_t *pulFaultStackAddress )
+{
+/* These are volatile to try and prevent the compiler/linker optimising them
+away as the variables never actually get used.  If the debugger won't show the
+values of the variables, make them global my moving their declaration outside
+of this function. */
+volatile uint32_t r0;
+volatile uint32_t r1;
+volatile uint32_t r2;
+volatile uint32_t r3;
+volatile uint32_t r12;
+volatile uint32_t lr; /* Link register. */
+volatile uint32_t pc; /* Program counter. */
+volatile uint32_t psr;/* Program status register. */
+
+    r0 = pulFaultStackAddress[ 0 ];
+    r1 = pulFaultStackAddress[ 1 ];
+    r2 = pulFaultStackAddress[ 2 ];
+    r3 = pulFaultStackAddress[ 3 ];
+
+    r12 = pulFaultStackAddress[ 4 ];
+    lr = pulFaultStackAddress[ 5 ];
+    pc = pulFaultStackAddress[ 6 ];
+    psr = pulFaultStackAddress[ 7 ];
+
+    /* When the following line is hit, the variables contain the register values. */
+    for( ;; );
+}
+
+void _MemManage_exception(void) {
+/*lint -restore*/
+
+  while (true) {
+  }
+}
+
 #if !defined(__DOXYGEN__)
 extern uint32_t __main_stack_end__;
 void Reset_Handler(void);
 void NMI_Handler(void) __attribute__((weak, alias("_unhandled_exception")));
-void HardFault_Handler(void) __attribute__((weak, alias("_unhandled_exception")));
-void MemManage_Handler(void) __attribute__((weak, alias("_unhandled_exception")));
+void HardFault_Handler(void) __attribute__((weak, alias("_HardFault_exception")));
+void MemManage_Handler(void) __attribute__((weak, alias("_MemManage_exception")));
 void BusFault_Handler(void) __attribute__((weak, alias("_unhandled_exception")));
 void UsageFault_Handler(void) __attribute__((weak, alias("_unhandled_exception")));
 void Vector1C(void) __attribute__((weak, alias("_unhandled_exception")));
